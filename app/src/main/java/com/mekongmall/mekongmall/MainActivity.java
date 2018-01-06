1400efaa322c267338b386fe7d60e299ee458b7a
KoingDev <sea.saingkoing@gmail.com>
2018-01-07 01:35:04 +0700

    implement retry button when no internet

--- app/src/main/java/com/mekongmall/mekongmall/MainActivity.java
+++ app/src/main/java/com/mekongmall/mekongmall/MainActivity.java
@@ -2,14 +2,21 @@ package com.mekongmall.mekongmall;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.onesignal.OneSignal;
 import com.wang.avi.AVLoadingIndicatorView;
@@ -22,8 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private AVLoadingIndicatorView indicatorView;
-    private String urlFromNotification;
-    private final String MAIN_URL = "http://mekong-mall.com/";
+    private ImageView sadCloud;
+    private ImageButton btnRetry;
+    private TextView noInternetDetail;
+    private TextView noInternet;
+
+    private String url = "http://mekong-mall.com/";
+    private boolean clearHistory = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,24 +49,36 @@ public class MainActivity extends AppCompatActivity {
                 .init();
 
         setContentView(R.layout.activity_main);
-
         indicatorView = findViewById(R.id.avi);
-
         webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new CustomWebViewClient(indicatorView));
+        sadCloud = findViewById(R.id.sad_cloud);
+        btnRetry = findViewById(R.id.retry);
+        noInternet = findViewById(R.id.no_internet);
+        noInternetDetail = findViewById(R.id.no_internet_detail);
+
+        webView.setWebViewClient(new CustomWebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
         webView.clearCache(true);
+        webView.clearHistory();
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
 
         // Get notification data from SplashActivity
         Bundle extra = getIntent().getExtras();
         if (extra != null) {
-            urlFromNotification = extra.getString("urlFromNotification");
-            webView.loadUrl(urlFromNotification);
-        } else {
-            webView.loadUrl(MAIN_URL);
+            url = extra.getString("urlFromNotification");
         }
+        webView.loadUrl(url);
+    }
+
+    public void retry(View view) {
+        webView.setVisibility(View.VISIBLE);
+        sadCloud.setVisibility(View.INVISIBLE);
+        btnRetry.setVisibility(View.INVISIBLE);
+        noInternet.setVisibility(View.INVISIBLE);
+        noInternetDetail.setVisibility(View.INVISIBLE);
+        webView.loadUrl(url);
+        clearHistory = true;
     }
 
     @Override
@@ -64,6 +88,40 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            // Show nothing when page error
+            webView.loadUrl("");
+            webView.setVisibility(View.INVISIBLE);
+            sadCloud.setVisibility(View.VISIBLE);
+            btnRetry.setVisibility(View.VISIBLE);
+            noInternet.setVisibility(View.VISIBLE);
+            noInternetDetail.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            indicatorView.show();
+        }
+
+        @Override
+        public void onPageCommitVisible(WebView view, String url) {
+            // Hide loading indicator when page is loading
+            indicatorView.hide();
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            if (clearHistory) {
+                clearHistory = false;
+                webView.clearHistory();
+                webView.clearCache(true);
+            }
+            super.onPageFinished(view, url);
+        }
     }
 
 }
